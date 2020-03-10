04406df2cfa5f89e8d7bc5801876f211766631bd
Angel Prieto <adps96@gmail.com>
2020-03-10 15:35:02 -0400

    Independent tests and examples for c++ and icpc code writing and compiling, as well as working examples for adamecius lsqt

---
+++ independent_testing/gsl_test/bessel_functions_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  float x = 1.0;
+  /*for (int i = 0; i <= 3; i++){
+    cout << "J_"<< i << "(" << x << ") = " << cyl_bessel_j(i, x) << endl;
+  }
+  */
+  cout << __cplusplus << endl;
+  //cout << sqrt(4.0) << endl;
+}
