cd184342d292f5edc7fb563eefacbbad37237531
Angel Prieto <adps96@gmail.com>
2020-03-10 19:31:52 -0400

    Bessel J function implementation with the intel math library

--- src/special_functions.cpp
+++ src/special_functions.cpp
@@ -1,7 +1,18 @@
 #include "special_functions.hpp"
 
-double chebyshev::besselJ(const int n, const double x){
-  assert(__cplusplus>=201703);
-  double y = std::cyl_bessel_j(n, x);
+double chebyshev::besselJ(const int n, const double x) {
+  double y;
+
+  switch (n) {
+  case 0:
+    y = j0(x);
+    break;
+  case 1:
+    y = j1(x);
+    break;
+  default:
+    y = jn(n, x);
+  }
+
   return y;
 }
